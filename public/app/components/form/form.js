6e222a112374c32e3f3a6fdc9f72636e0fe6a11d
gabriel <gabe.wilk@gmail.com>
2018-12-03 11:13:38 -0500

    form

--- public/app/components/form/form.js
+++ public/app/components/form/form.js
@@ -3,11 +3,31 @@ const submit = {
     templateUrl: "app/components/form/form.html",
     controller: ["MainService", function(MainService){
         const vm = this;
+        vm.lowImportance = false;
+        vm.medImportance = false;
+        vm.highImportance = false; 
 
-        vm.saveTask = (newTask) => {
-            console.log(newTask);
-            MainService.postTodo(newTask);
+        vm.saveTask = (task) => {
+            console.log(task);
+
+            //console.log("test");
+            MainService.postTodo(task);
         };
+
+        vm.taskImportance = (value) => {
+            MainService.put(value);
+        }
+
+
+        vm.medImportance = () => {
+            vm.medImportance = !vm.medImportance;
+        }
+        vm.flipHigh = () => {
+            vm.highImportance = !vm.highImportance;
+        }
+        vm.flipLow = () => {
+            vm.lowImportance = !vm.lowImportance;
+        }
     }]
 }
 
