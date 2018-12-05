8b132c2120b02d41cb09b0ff6bbf78949a8322f1
Karissa <phillips.karissa00@gmail.com>
2018-12-05 10:42:39 -0500

    shopping component added to home page

---
+++ public/app/components/shopping/shopping.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const shopping ={
+    templateUrl: "app/components/shopping/shopping.html",
+    controller: [function(){
+        const vm = this;
+
+    }]
+
+
+
+}
+
+angular 
+    .module("App")
+    .component("shopping", shopping);
