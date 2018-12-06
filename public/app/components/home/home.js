52da0399d06e677a580d07b381934f8b63eac9ba
gabriel <gabe.wilk@gmail.com>
2018-12-06 16:30:06 -0500

    test

--- public/app/components/home/home.js
+++ public/app/components/home/home.js
@@ -7,16 +7,21 @@ const home = {
         //getter function, retrieves values from a promise and sends them to the service. Then, we ask the service what our mood is depending on these two values. 
         vm.getMood = () => {
             MainService.getTasks().then((result) => {
+                console.log(result.data.length);
                 MainService.setAllTasks(result.data.length);
             });
+          
             MainService.getCompletedTasks().then((result) => {
                 MainService.setCompTasks(result.data.length);
             })
-            vm.allTasks = MainService.getRealTasks();
-            vm.compTasks = MainService.getRealCompTasks();
-            vm.mood = MainService.setMood(vm.compTasks,vm.allTasks);
+            
+            let allTasks = MainService.getRealTasks();
+            let compTasks = MainService.getRealCompTasks();
+            vm.mood = MainService.setMood(compTasks, allTasks);
             console.log(vm.mood);
+            return vm.mood;
         }
+
         vm.getMood();
 
     }]
